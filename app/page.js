644ab7d15ec286d9991c1b5c906bd35ac17b5201
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-11-22 11:06:10 +0700

    add cta

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Cta from "@/components/Cta";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Services />
       <Work />
+      <Cta />
     </main>
   );
 }
