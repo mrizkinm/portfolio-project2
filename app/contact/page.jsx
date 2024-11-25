b773414ca3387d15d233678b36f3673af1d34615
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-11-25 21:12:02 +0700

    add contact and projects

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,47 @@
+import DevImg from '@/components/DevImg'
+import Form from '@/components/Form'
+import { MailIcon, HomeIcon, PhoneCall } from 'lucide-react'
 import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <div className="container mx-auto px-5">
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px]">
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+              <span className="w-[30px] h-[2px] bg-primary"></span>
+
+              <div>Hello</div>
+            </div>
+            <h1 className="h1 max-w-md mb-8">Lets Work Together</h1>
+            <p className="subtitle max-w-[400px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </p>
+          </div>
+          <div className="hidden xl:flex justify-end items-center">
+            <DevImg />
+          </div>
+        </div>
+        <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+            <div className="flex items-center gap-x-8">
+              <MailIcon size={18} className="text-primary" />
+              <div>email@gmail.com</div>
+            </div>
+            <div className="flex items-center gap-x-8">
+              <HomeIcon size={18} className="text-primary" />
+              <div>Jalan kemerdekaan</div>
+            </div>
+            <div className="flex items-center gap-x-8">
+              <PhoneCall size={18} className="text-primary" />
+              <div>+656545444776</div>
+            </div>
+          </div>
+          <Form />
+        </div>
+      </div>
+    </section>
   )
 }
 
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,8 +1,92 @@
-import React from 'react'
+"use client"
+
+import ProjectCard from '@/components/ProjectCard'
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs'
+import React, { useState } from 'react'
+
+const project = [
+  {
+    image: '/project1.jpg',
+    category: 'React JS',
+    name: 'Website bagus',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '/',
+    github: '/'
+  },
+  {
+    image: '/project2.jpg',
+    category: 'Laravel',
+    name: 'Another Website bagus',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '/',
+    github: '/'
+  },
+  {
+    image: '/project3.jpg',
+    category: 'React JS',
+    name: 'Website bagus lagi',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '/',
+    github: '/'
+  },
+  {
+    image: '/project4.jpg',
+    category: 'Next JS',
+    name: 'Website lagi',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '/',
+    github: '/'
+  },
+  {
+    image: '/project1.jpg',
+    category: 'Codeigniter',
+    name: 'Code web',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '/',
+    github: '/'
+  },
+  {
+    image: '/project2.jpg',
+    category: 'Next JS',
+    name: 'Portofolio',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '/',
+    github: '/'
+  }
+];
+
+const uniqueCat = ['all projects', ...new Set(project.map((item) => item.category))];
+
 
 const Projects = () => {
+  const [categoryList, setCategoryList] = useState(uniqueCat)
+  const [category, setCategory] = useState('all projects')
+  const filteredProjects = project.filter((item) => {
+    return category === 'all projects'
+    ? item
+    : item.category === category;
+  });
+
   return (
-    <div>Projects</div>
+    <section className="min-h-screen pt-12">
+      <div className="container mx-auto px-5">
+        <h2 className="text-4xl font-bold mb-8 xl:mb-16 text-center mx-auto">Projects</h2>
+        <Tabs defaultValue={category} className="mb-24 xl:mb-48">
+          <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
+            {categoryList.map((item, index) => (
+              <TabsTrigger onClick={() => setCategory(item)} value={item} key={index} className="capitalize w-[162px] md:w-auto">{item}</TabsTrigger>
+            ))}
+          </TabsList>
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((item, index) => (
+              <TabsContent value={category} key={index}>
+                <ProjectCard project={item} />
+              </TabsContent>
+            ))}
+          </div>
+        </Tabs>
+      </div>
+    </section>
   )
 }
 
