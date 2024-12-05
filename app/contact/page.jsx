7c7a82f700e0b49e54b716e155b3033039758dd3
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-05 16:02:13 +0700

    change phone

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -11,7 +11,6 @@ const Contact = () => {
           <div className="flex flex-col justify-center">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
               <span className="w-[30px] h-[2px] bg-primary"></span>
-
               <div>Hello</div>
             </div>
             <h1 className="h1 max-w-md mb-8">Lets Work Together</h1>
@@ -35,7 +34,7 @@ const Contact = () => {
             </div>
             <div className="flex items-center gap-x-8">
               <PhoneCall size={18} className="text-primary" />
-              <div>+656545444776</div>
+              <div>+6285678765432</div>
             </div>
           </div>
           <Form />
